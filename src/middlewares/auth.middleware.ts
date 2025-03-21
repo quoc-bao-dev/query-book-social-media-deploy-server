1b95eabd581f2785bcf6397ca2f939c0ee69cd41
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-03-21 17:08:21 +0700

    fix: update start script path in package.json and add debug logs in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -8,6 +8,10 @@ export const authMiddleware = (
     const apiKey = req.headers['x-api-key'];
 
     const checkApiKey = apiKey === process.env.DEPLOY_API_KEY;
+    console.log('checkApiKey', checkApiKey);
+    console.log('apiKey', apiKey);
+    console.log('process.env.DEPLOY_API_KEY', process.env.DEPLOY_API_KEY);
+
     if (checkApiKey) {
         next();
     } else {
